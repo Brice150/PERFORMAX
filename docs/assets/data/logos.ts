c275ef7f143452b5f3be86492a81f365c93f9ba0
brice150 <brice.lecomte0@gmail.com>
2025-10-02 10:37:22 +0200

    new build

---
+++ docs/assets/data/logos.ts
@@ -0,0 +1,11 @@
+export const logos: string[] = [
+  'bx bx-football',
+  'bx bx-basketball',
+  'bx bx-tennis-ball',
+  'bx bx-dumbbell',
+  'bx bx-run',
+  'bx bx-swim',
+  'bx bx-cycling',
+  'bx bx-trophy',
+  'bx bx-medal',
+];
